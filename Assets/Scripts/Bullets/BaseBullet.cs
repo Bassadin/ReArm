b9b00df02b0dc7e65128dd9c56e13558da659fb9
Bastian Hodapp <bastianhodapp@gmail.com>
2019-10-11 21:35:26 +0200

    Added hard death timer for bullets

---
+++ Assets/Scripts/Bullets/BaseBullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBullet : MonoBehaviour
+{
+    void Start()
+    {
+        //Delete every bullet after 20 seconds by default
+        Destroy(gameObject, 20);
+    }
+}
