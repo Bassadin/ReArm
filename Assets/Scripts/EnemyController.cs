74925f79888e3547fe1f85ccbb0b5ec5c378f196
Bastian Hodapp <bastianhodapp@gmail.com>
2019-10-18 14:27:40 +0200

    Added first version of enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float minSpeed = 10;
+    public float maxSpeed = 15;
+
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = UnityEngine.Random.Range(minSpeed, maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
