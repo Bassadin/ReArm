bd6d3779d98c705425cf56b54856e78356866205
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-08 15:05:29 +0100

    Added more buttons

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeSceneButton : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
     public void LoadScene(int level) {
         SceneManager.LoadScene(level);
