66c8766105b9e8c402f4152ee7a34c5361dccf94
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-08 14:55:58 +0100

    Add Main Menu with Navigation

---
+++ Assets/ChangeSceneButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneButton : MonoBehaviour
+{
+    public void LoadScene(int level) {
+        SceneManager.LoadScene(level);
+    }
+}
