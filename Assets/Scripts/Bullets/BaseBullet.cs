d4a7436d933ba1107b3f475104364d5fca0410b8
Bastian Hodapp <bastianhodapp@gmail.com>
2019-12-13 14:36:24 +0100

    Removed console log

--- Assets/Scripts/Bullets/BaseBullet.cs
+++ Assets/Scripts/Bullets/BaseBullet.cs
@@ -12,7 +12,6 @@ public class BaseBullet : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Enemy")) {
-            Debug.Log("bullet collision detected!");
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
