cd650a4a84fa7e0bf9d008dd9c5d531a755adacd
Bastian Hodapp <bastianhodapp@gmail.com>
2019-12-16 16:06:49 +0100

    Made bullets get smaller as they travel

--- Assets/Scripts/Bullets/BaseBullet.cs
+++ Assets/Scripts/Bullets/BaseBullet.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BaseBullet : MonoBehaviour
 {
+    private Transform _transform;
+
+    private Vector3 baseScale;
+    
     void Start()
     {
         //Delete every bullet after 20 seconds by default
@@ -15,5 +19,16 @@ public class BaseBullet : MonoBehaviour
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
+    }
+
+    void Awake() {
+        _transform = this.transform;
+        baseScale = _transform.localScale;
+    }
+
+    void Update() {
+        //Scale enemies bigger as they move downwards
+        float addedScaleSize = _transform.position.y * -0.025f;
+        _transform.localScale = new Vector3(baseScale.x + addedScaleSize, baseScale.y + addedScaleSize, 0);
     }
 }
