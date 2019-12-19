0010d68e5efe260bf3c832888966c2b55baf15fa
Bastian Hodapp <bastianhodapp@gmail.com>
2019-12-19 19:02:18 +0100

    Added touch controls

---
+++ Assets/Scripts/DragFingerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragFingerMove : MonoBehaviour
+{
+    private Vector2 touchPosition;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    private float moveSpeed = 10f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            direction = (touchPosition - (Vector2)transform.position);
+            rb.velocity = new Vector2(direction.x, 0) * moveSpeed;
+
+            if (touch.phase == TouchPhase.Ended)
+                rb.velocity = Vector2.zero;
+        }
+    }
+}
+
