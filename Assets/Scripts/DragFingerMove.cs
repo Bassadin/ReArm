f112cccbab87f3350dff8057f534776e1c0a12a5
Bastian Hodapp <bastianhodapp@gmail.com>
2019-12-19 19:13:11 +0100

    Added touch region for character control
    In lower third of screen

--- Assets/Scripts/DragFingerMove.cs
+++ Assets/Scripts/DragFingerMove.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class DragFingerMove : MonoBehaviour
 {
     private Vector2 touchPosition;
-    private Rigidbody2D rb;
+    private Rigidbody2D rigidbody2d;
     private Vector2 direction;
     private float moveSpeed = 10f;
 
     // Use this for initialization
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,12 +21,18 @@ public class DragFingerMove : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            direction = (touchPosition - (Vector2)transform.position);
-            rb.velocity = new Vector2(direction.x, 0) * moveSpeed;
+            if (touch.position.y < Screen.height / 3) //Only recognize touches in the lower third of the screen
+            {
+                touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                direction = (touchPosition - (Vector2)transform.position);
+                rigidbody2d.velocity = new Vector2(direction.x, 0) * moveSpeed;
 
-            if (touch.phase == TouchPhase.Ended)
-                rb.velocity = Vector2.zero;
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    rigidbody2d.velocity = Vector2.zero;
+                }
+            }
+
         }
     }
 }
