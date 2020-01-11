84c5dcaa615aaa6637f5d911318d387196bd924d
Bastian Hodapp <bastianhodapp@gmail.com>
2020-01-11 20:09:54 +0100

    Pause Menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+
+    public GameObject pauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+    }
+}
